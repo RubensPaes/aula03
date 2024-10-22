e35ea7f7a1ac177ce7c01a76363a31cb48f8f3c5
RubensPaes <rubenspaesneto@gmail.com>
2024-10-22 19:16:53 -0400

    update final
    syvfysdgfyseg

--- src/pages/ListarProdutos.jsx
+++ src/pages/ListarProdutos.jsx
@@ -2,7 +2,7 @@ export default function ListarProdutos({ id, title, description, price, image })
     return (
         <div>
             <ul>
-                <li key={id}>
+                <li>
                     <h2>{title}</h2>
                     <p>{description}</p>
                     <p>Preço: R${price}</p>
